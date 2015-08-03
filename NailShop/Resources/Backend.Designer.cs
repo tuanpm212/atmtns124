c876f9eb6ce139902a6ed346bd836bb4cc712aeb
Tuấn Phạm <tuanpm212@gmail.com>
2015-08-04 00:14:08 +0700

    08/04/2015

--- NailShop/Resources/Backend.Designer.cs
+++ NailShop/Resources/Backend.Designer.cs
@@ -79,6 +79,24 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Add Photo.
+        /// </summary>
+        public static string btnAddPhoto {
+            get {
+                return ResourceManager.GetString("btnAddPhoto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Back.
+        /// </summary>
+        public static string btnBack {
+            get {
+                return ResourceManager.GetString("btnBack", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Cancel.
         /// </summary>
         public static string btnCancel {
@@ -115,11 +133,29 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Search.
+        /// </summary>
+        public static string btnSearch {
+            get {
+                return ResourceManager.GetString("btnSearch", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Sign In.
         /// </summary>
         public static string btnSignIn {
             get {
                 return ResourceManager.GetString("btnSignIn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Choose Store.
+        /// </summary>
+        public static string chooseStore {
+            get {
+                return ResourceManager.GetString("chooseStore", resourceCulture);
             }
         }
         
@@ -133,11 +169,146 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to ALBUMS LIST.
+        /// </summary>
+        public static string headAlbumsList {
+            get {
+                return ResourceManager.GetString("headAlbumsList", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to BRAND LIST.
+        /// </summary>
+        public static string headBrand {
+            get {
+                return ResourceManager.GetString("headBrand", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Create/View Brand.
+        /// </summary>
+        public static string headCreateBrand {
+            get {
+                return ResourceManager.GetString("headCreateBrand", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Create/View Photo.
+        /// </summary>
+        public static string headCreatePhoto {
+            get {
+                return ResourceManager.GetString("headCreatePhoto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Create/View Product Hot.
+        /// </summary>
+        public static string headCreateProductHot {
+            get {
+                return ResourceManager.GetString("headCreateProductHot", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Create/View Promotion.
+        /// </summary>
+        public static string headCreatePromotion {
+            get {
+                return ResourceManager.GetString("headCreatePromotion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Create/View Slider.
+        /// </summary>
+        public static string headCreateSlider {
+            get {
+                return ResourceManager.GetString("headCreateSlider", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Create/View Video.
+        /// </summary>
+        public static string headCreateVideo {
+            get {
+                return ResourceManager.GetString("headCreateVideo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to PRODUCT HOT.
+        /// </summary>
+        public static string headProductHot {
+            get {
+                return ResourceManager.GetString("headProductHot", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to SLIDER.
+        /// </summary>
+        public static string headSlide {
+            get {
+                return ResourceManager.GetString("headSlide", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to VIDEO LIST.
+        /// </summary>
+        public static string headVideoList {
+            get {
+                return ResourceManager.GetString("headVideoList", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Welcome.
+        /// </summary>
+        public static string headWelcome {
+            get {
+                return ResourceManager.GetString("headWelcome", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Active.
+        /// </summary>
+        public static string lblActive {
+            get {
+                return ResourceManager.GetString("lblActive", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Brand Url.
+        /// </summary>
+        public static string lblBrandUrl {
+            get {
+                return ResourceManager.GetString("lblBrandUrl", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to City.
         /// </summary>
         public static string lblCity {
             get {
                 return ResourceManager.GetString("lblCity", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Code.
+        /// </summary>
+        public static string lblCode {
+            get {
+                return ResourceManager.GetString("lblCode", resourceCulture);
             }
         }
         
@@ -151,11 +322,128 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Discount Percent(%).
+        /// </summary>
+        public static string lblDiscountPercent {
+            get {
+                return ResourceManager.GetString("lblDiscountPercent", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to From Date.
+        /// </summary>
+        public static string lblFromDate {
+            get {
+                return ResourceManager.GetString("lblFromDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Image.
+        /// </summary>
+        public static string lblImage {
+            get {
+                return ResourceManager.GetString("lblImage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Image cover.
+        /// </summary>
+        public static string lblImageCover {
+            get {
+                return ResourceManager.GetString("lblImageCover", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Image photo.
+        /// </summary>
+        public static string lblImagePhoto {
+            get {
+                return ResourceManager.GetString("lblImagePhoto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Long Description.
+        /// </summary>
+        public static string lblLongDescription {
+            get {
+                return ResourceManager.GetString("lblLongDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Name.
+        /// </summary>
+        public static string lblName {
+            get {
+                return ResourceManager.GetString("lblName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Password.
         /// </summary>
         public static string lblPassword {
             get {
                 return ResourceManager.GetString("lblPassword", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Price.
+        /// </summary>
+        public static string lblPrice {
+            get {
+                return ResourceManager.GetString("lblPrice", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Priority.
+        /// </summary>
+        public static string lblPriority {
+            get {
+                return ResourceManager.GetString("lblPriority", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Product Name.
+        /// </summary>
+        public static string lblProductName {
+            get {
+                return ResourceManager.GetString("lblProductName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Promotion Name.
+        /// </summary>
+        public static string lblPromotionName {
+            get {
+                return ResourceManager.GetString("lblPromotionName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Quantity.
+        /// </summary>
+        public static string lblQuantity {
+            get {
+                return ResourceManager.GetString("lblQuantity", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Short Description.
+        /// </summary>
+        public static string lblShortDescription {
+            get {
+                return ResourceManager.GetString("lblShortDescription", resourceCulture);
             }
         }
         
@@ -178,6 +466,33 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Title.
+        /// </summary>
+        public static string lblTitle {
+            get {
+                return ResourceManager.GetString("lblTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to To Date.
+        /// </summary>
+        public static string lblToDate {
+            get {
+                return ResourceManager.GetString("lblToDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Url link.
+        /// </summary>
+        public static string lblUrlLink {
+            get {
+                return ResourceManager.GetString("lblUrlLink", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Email/Login Name.
         /// </summary>
         public static string lblUserName {
@@ -187,11 +502,56 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Video.
+        /// </summary>
+        public static string lblVideo {
+            get {
+                return ResourceManager.GetString("lblVideo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to ZipCode.
         /// </summary>
         public static string lblZipCode {
             get {
                 return ResourceManager.GetString("lblZipCode", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Are you sure to delete?.
+        /// </summary>
+        public static string messDelete {
+            get {
+                return ResourceManager.GetString("messDelete", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Photo Detail.
+        /// </summary>
+        public static string messPhotoDetail {
+            get {
+                return ResourceManager.GetString("messPhotoDetail", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You save sucessfull!.
+        /// </summary>
+        public static string mesSuccess {
+            get {
+                return ResourceManager.GetString("mesSuccess", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Warring.
+        /// </summary>
+        public static string mesTitleWarring {
+            get {
+                return ResourceManager.GetString("mesTitleWarring", resourceCulture);
             }
         }
         
